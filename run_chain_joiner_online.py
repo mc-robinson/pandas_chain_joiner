0a4246117792b16b66017844b73b7179b7d12dc3
robimc14 <matthew.robinson@yale.edu>
2017-06-11 23:23:36 -0400

    implemented CLI using argparse

--- run_chain_joiner_online.py
+++ run_chain_joiner_online.py
@@ -14,32 +14,86 @@ usage: python run_chain_joiner.py pdb_code
 import os
 import sys
 import subprocess
+import argparse
 
-pdb_id = sys.argv[1]
+def main():
 
-#make url's for wget
-pdb_url = 'https://files.rcsb.org/download/' + pdb_id + '.pdb'
-fasta_url = 'http://www.rcsb.org/pdb/files/fasta.txt?structureIdList=' + pdb_id
+    #create parser object
+    parser = argparse.ArgumentParser(
+        prog='run_chain_joiner_online.py',
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description="""
+    SCRIPT TO FIX MISSING RESIDUES IN PDB FILES.
+    This version obtains the PDB and FASTA files 
+    for the user directly from the web.
 
-#make file names
-pdb_fn = pdb_id + '.pdb'
-fasta_fn = pdb_id + '.fasta'
+    @author: Matthew Robinson matthew.robinson@yale.edu
+    @author: William L. Jorgensen Lab 
+    
+    REQUIREMENTS:
+    Preferably Anaconda python 3 environment with following modules:
+    
+    Modeller
+    pandas 
+    biopandas
+    argparse
+    getopt
+    numpy
+    """)
 
-#get the files from the pdb_database
-os.system('wget -O ' + pdb_fn + ' ' + pdb_url)
-os.system('wget -O ' + fasta_fn + ' ' + fasta_url)
+    #defining arguments for parser object
+    parser.add_argument('pdb_id')
+    parser.add_argument(
+        "-a", "--automodel", help="The simplest method for simple comparitive modeling.")
+    parser.add_argument(
+        "-l", "--loopmodel", help="Builds a model by refining the loop with the missing residues.")
+    parser.add_argument(
+        "-f", "--fixed_automodel", help="Builds an automodel and keeps the non-missing residues fixed.")
 
-#call the functions
-subprocess.call(['python', 'pandas_chain_joiner.py', pdb_id + '.pdb', pdb_id + '.fasta','-a'])
+    #parse the arguments from standard input
+    args = parser.parse_args()
 
-#make a folder for the output
-os.makedirs('./' + pdb_id +'_output/')
+    #pdb_id = sys.argv[1]
+    pdb_id = str(args.pdb_id)
 
-#get a list of all output files in the working directory
-output_files = [filename for filename in os.listdir('.') if filename.startswith(pdb_id)]
+    #make url's for wget
+    pdb_url = 'https://files.rcsb.org/download/' + pdb_id + '.pdb'
+    fasta_url = 'http://www.rcsb.org/pdb/files/fasta.txt?structureIdList=' + pdb_id
 
-#mv these files to the output folder
-for file in output_files:
-	os.system('mv ' + file + ' ./' + pdb_id + '_output/')	
+    #make file names
+    pdb_fn = pdb_id + '.pdb'
+    fasta_fn = pdb_id + '.fasta'
 
+    #get the files from the pdb_database
+    os.system('wget -O ' + pdb_fn + ' ' + pdb_url)
+    os.system('wget -O ' + fasta_fn + ' ' + fasta_url)
 
+    #call the functions based on flag
+    if args.automodel:
+        subprocess.call(['python', 'pandas_chain_joiner.py', pdb_id + '.pdb', pdb_id + '.fasta','-a'])
+
+    if args.loopmodel:
+        subprocess.call(['python', 'pandas_chain_joiner.py', pdb_id + '.pdb', pdb_id + '.fasta','-l'])
+
+    elif args.fixed_automodel:
+        subprocess.call(['python', 'pandas_chain_joiner.py', pdb_id + '.pdb', pdb_id + '.fasta','-f'])
+
+    else:
+        subprocess.call(['python', 'pandas_chain_joiner.py', pdb_id + '.pdb', pdb_id + '.fasta','-a'])
+
+    #make a folder for the output
+    os.makedirs('./' + pdb_id +'_output/')
+
+    #get a list of all output files in the working directory
+    output_files = [filename for filename in os.listdir('.') if filename.startswith(pdb_id)]
+
+    #mv these files to the output folder
+    for file in output_files:
+        try:
+            os.system('mv ' + file + ' ./' + pdb_id + '_output/')   
+        except:
+            pass
+
+if __name__ == "__main__":
+    # calling the main function
+    main()

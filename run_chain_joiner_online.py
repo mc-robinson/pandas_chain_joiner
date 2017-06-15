01daa73f5c7c87734e1350b09583a20ffd5cc065
robimc14 <matthew.robinson@yale.edu>
2017-06-15 11:53:05 -0400

    updated URL for wget

--- run_chain_joiner_online.py
+++ run_chain_joiner_online.py
@@ -58,7 +58,8 @@ def main():
 
     #make url's for wget
     pdb_url = 'https://files.rcsb.org/download/' + pdb_id + '.pdb'
-    fasta_url = 'http://www.rcsb.org/pdb/files/fasta.txt?structureIdList=' + pdb_id
+    #fasta_url = 'http://www.rcsb.org/pdb/download/viewFastaFiles.do?structureIdList=' + pdb_id + '&compressionType=uncompressed'
+    fasta_url = 'http://www.rcsb.org/pdb/download/viewFastaFiles.do?structureIdList=' + pdb_id
 
     #make file names
     pdb_fn = pdb_id + '.pdb'
@@ -66,7 +67,9 @@ def main():
 
     #get the files from the pdb_database
     os.system('wget -O ' + pdb_fn + ' ' + pdb_url)
-    os.system('wget -O ' + fasta_fn + ' ' + fasta_url)
+    os.system('wget -O ' + fasta_fn +'.gz ' + fasta_url + ' && gzip -d '+ fasta_fn +'.gz')
+    #os.system('wget -q -O - ' + fasta_url + ' | gzip -d > ' + fasta_fn)
+    #os.system('gzip -d ' + fasta_fn + '.gz')
 
     #call the functions based on flag
     if args.automodel:
